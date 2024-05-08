4ebe2efc3fe8b8cfe5df5d715e3773234e62bbf1
olich3003 <121357123+olich3003@users.noreply.github.com>
2024-05-08 16:39:55 +0300

    new_bot.py

--- new_bot.py
+++ new_bot.py
@@ -51,8 +51,10 @@ async def start(update, context):
 
 
 async def text_handler(update, context):
-    global HOD
     current_user = users[update.effective_user.id]
+    if current_user.status == 'gaming':
+        await game_text_handler(update, context)
+        return
     user_messege = update.message.text
     last_user_messege = context.chat_data['last']
     back = context.chat_data['back']
@@ -93,8 +95,12 @@ async def text_handler(update, context):
         room_id = str(uuid.uuid4())
         create_room(context.user_data['n_of_players'], context.user_data['move_time'], room_id, bot,  current_user)
         text = f"Вы успешно создали игровую комнату!\nКод для комнаты: `{room_id}`\nСообщи его друзьям, чтобы они могли подключиться к игре"
-        keyboard = [["Начать игру"], ["Удалить комнату"]]
+        keyboard = [["Удалить комнату"]]
         reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+        print(current_user.cur_room)
+        await update.message.reply_text(text=text, reply_markup=reply_markup, parse_mode='Markdown')
+        await current_user.cur_room.waiting_for_players(update, context)
+        return
 
     ########################################################################################
     if user_messege == 'Главное меню':
@@ -142,38 +148,106 @@ async def text_handler(update, context):
             await current_user.cur_room.get_field(update, context)
         return
     ##################################################
-    if user_messege == 'Бросить кубики':
-        if DEBAG:
-            current_user.last_dices_value = hods[HOD]
-            current_user.cell += hods[HOD]
-            HOD = HOD + 1
-        else:
-            dice_1 = await context.bot.send_dice(chat_id=current_user.chat_id)
-            dice_2 = await context.bot.send_dice(chat_id=current_user.chat_id)
-            current_user.last_dices_value = dice_1.dice.value + dice_2.dice.value
-            current_user.cell += (dice_1.dice.value + dice_2.dice.value)
-        if current_user.cell // 40 == 1:
-            current_user.budget += 200
-        current_user.cell = current_user.cell % 40
-        context.chat_data['last'] = user_messege
-        await current_user.cur_room.update_message(update, context)
-        return
-    if user_messege == 'Купить':
-        context.chat_data['last'] = user_messege
-        await current_user.cur_room.update_message(update, context)
-        return
-    if user_messege == 'Закончить ход':
-        context.chat_data['last'] = user_messege
-        await current_user.cur_room.step(update, context)
-        return
-    if user_messege == 'Заплатить ренту':
-        context.chat_data['last'] = user_messege
-        await current_user.cur_room.update_message(update, context)
-        return
+
     context.chat_data['last'] = user_messege
     await update.message.reply_text(text=text, reply_markup=reply_markup, parse_mode='Markdown')
 
 
+
+async def game_text_handler(update, context):
+    global HOD
+    current_user = users[update.effective_user.id]
+    user_messege = update.message.text
+    last_user_messege = context.chat_data['last']
+    context.chat_data['back'] = None
+
+
+    if context.chat_data['last'] == 'В тюрьме': #если игрок в тюрьме
+        if user_messege == 'Остаться в тюрьме': 
+            context.chat_data['last'] = user_messege
+            await current_user.cur_room.step(update, context)
+            return
+        if user_messege == 'Использовать карточку': # выход из тюрьмы
+            current_user.jail_free = False
+        current_user.prisoner = False
+        current_user.jail_doubles = False
+        context.chat_data['last'] = 'Бросить кубики'
+        await current_user.cur_room.update_message(update, context)
+        return
+    
+    
+    if context.chat_data['last'] == 'Купить дом':
+        context.chat_data['last'] = user_messege
+        await current_user.cur_room.buing_house(update, context, current_user)
+        return
+        
+
+    if user_messege == 'Моё имущество':
+        await current_user.cur_room.send_property(update, context, current_user)
+        return
+    
+    if user_messege == 'Все карточки':
+        await current_user.cur_room.send_cards(update, context, current_user)
+        return
+    
+    if user_messege == 'Бросить кубики':
+        context.chat_data['last'] = user_messege
+        if DEBAG: # режим дебага
+            current_user.last_dices_value = hods[HOD]
+            current_user.cell = hods[HOD]
+            HOD = HOD + 1
+            if current_user.prisoner:
+                context.chat_data['last'] = 'В тюрьме'
+                current_user.jail_doubles = True
+                current_user.jail_free = True
+            if current_user.cell == 30: # если попал на поле "отправляйся в тюрьму"
+                current_user.cur_room.put_in_jail(current_user, 'cell')
+        else: # обычный режим
+            dice_1 = await context.bot.send_dice(chat_id=current_user.chat_id)
+            dice_2 = await context.bot.send_dice(chat_id=current_user.chat_id)
+            current_user.last_dices_value = dice_1.dice.value + dice_2.dice.value
+            current_user.cell += (dice_1.dice.value + dice_2.dice.value)
+
+            if dice_1.dice.value == dice_2.dice.value: # если выпали дубли
+                current_user.doubles += 1
+            else:
+                current_user.doubles = 0
+
+            if current_user.doubles == 3: # если выкинул 3 дубля подряд
+                current_user.cur_room.put_in_jail(current_user, 'doubles')
+
+            if current_user.cell == 30: # если попал на поле "отправляйся в тюрьму"
+                current_user.cur_room.put_in_jail(current_user, 'cell')
+
+            if current_user.prisoner: # если игрок в тюрьме
+                current_user.turns_in_prison += 1
+                if dice_1.dice.value == dice_2.dice.value:
+                    current_user.jail_doubles = True
+                context.chat_data['last'] = 'В тюрьме'    
+        if current_user.cell // 40 == 1:
+            current_user.budget += 200
+        current_user.cell = current_user.cell % 40
+        await current_user.cur_room.update_message(update, context)
+        return
+
+    if user_messege in ['Купить', 'Не покупать', 'Заплатить ренту']:
+        context.chat_data['last'] = user_messege
+        await current_user.cur_room.update_message(update, context)
+        return
+    
+    if user_messege == 'Открыть карточку':
+        context.chat_data['last'] = user_messege
+        await current_user.cur_room.update_message(update, context)
+    
+    if user_messege == 'Купить дом': # покупка дома
+        context.chat_data['last'] = user_messege
+        await current_user.cur_room.update_message(update, context)
+        return
+
+    if user_messege == 'Закончить ход':
+        context.chat_data['last'] = user_messege
+        await current_user.cur_room.step(update, context)
+        return
 
 
 bot.add_handler(CommandHandler('start', start))
